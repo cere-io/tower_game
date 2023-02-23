04d8e92240458854071cfab4cb74c5a32868f098
Sergey Kambalin <sergey.kambalin@cere.io>
2023-02-23 14:05:10 +0600

    Preloader -> Wallet -> Leaderboard flow

--- games-sdk.js
+++ games-sdk.js
@@ -2,7 +2,9 @@ const { GamesSDK } = require("@cere/games-sdk");
 
 window.CereGamesSdk = new GamesSDK({
   onReady: (sdk) => {
-    window.gamesSdkPreloader = sdk.showPreloader();
+    window.gamesSdkPreloader = sdk.showPreloader(() => {
+      document.getElementById('start').click();
+    });
   },
 });
 
