fea49b092e6620e0bfad3996c2939ba775fafb92
Sergey Kambalin <sergey.kambalin@cere.io>
2023-02-28 18:59:08 +0600

    Update SDK with support of Leaderboard API

--- games-sdk.js
+++ games-sdk.js
@@ -1,6 +1,7 @@
 const { GamesSDK } = require("@cere/games-sdk");
 
 window.CereGamesSdk = new GamesSDK({
+  gameId: 'tower-game',
   onReady: (sdk) => {
     window.gamesSdkPreloader = sdk.showPreloader(() => {
       document.getElementById('start')?.click();
