9fca3ca7aff047792a4eaad0c95208eb0b0ed29c
Sergey Kambalin <sergey.kambalin@cere.io>
2023-03-02 20:02:50 +0600

    Update Games SDK

--- games-sdk.js
+++ games-sdk.js
@@ -3,8 +3,10 @@ const { GamesSDK } = require("@cere/games-sdk");
 window.CereGamesSdk = new GamesSDK({
   gameId: 'tower-game',
   onReady: (sdk) => {
-    window.gamesSdkPreloader = sdk.showPreloader(() => {
-      document.getElementById('start')?.click();
+    window.gamesSdkPreloader = sdk.showPreloader({
+      onStart:() => {
+        document.getElementById('start')?.click();
+      }
     });
   },
 });
